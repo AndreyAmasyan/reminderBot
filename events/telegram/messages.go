2288b119a2e8bf29511826a62524d141f3436c2f
Andrey <amasyanandrey1@gmail.com>
2022-08-23 03:15:46 +0300

    Updated messages

--- events/telegram/messages.go
+++ events/telegram/messages.go
@@ -10,7 +10,7 @@ Caution! After that, this page will be removed from your list!`
 const msgHello = "Hi there! 👾\n\n" + msgHelp
 
 const (
-	msgUnknownCommand = "I LOVE YOU ANECHKA!!!!"
+	msgUnknownCommand = "Unknown command"
 	msgNoSavedPages   = "You have no saved pages 🙊"
 	msgSaved          = "Saved! 👌"
 	msgAlreadyExists  = "You have already have this page in your list 🤗"
